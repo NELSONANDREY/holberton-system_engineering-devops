5d856580f584f8df69c0ba4f349289147e402669
NELSONANDREY <3424@holbertonschool.com>
2022-04-19 22:48:14 -0500

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,3 +5,8 @@ file_line { 'Client configuration file':
   path =>  '/etc/ssh/ssh_config',
   line =>  'PasswordAuthentication no',
 }
+
+file_line {  'modo sad':
+  path =>  '/etc/ssh/ssh_config',
+  line =>  'IdentityFile ~/.ssh/school',
+}
